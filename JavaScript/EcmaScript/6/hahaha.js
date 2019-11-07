ee087de0ebc446b7fce3cecbf324c53e68dd8a54
Angutivik Casper Rúnur Tausen Hansen <runurth@gmail.com>
2019-11-07 16:25:49 +0100

    Added some things

--- JavaScript/EcmaScript/6/hahaha.js
+++ JavaScript/EcmaScript/6/hahaha.js
@@ -1,5 +1,47 @@
+// A classic for loop
 let ha = "";
 for(let i = 0; i < 3; i++){
 	ha += "ha";
+}
+console.log(ha);
+
+// A messed up nested for loop
+ha = "";
+for(let i = 0; i < 1; i++){
+	ha += "ha";
+	for(let i = 0; i < 1; i++){
+		ha += "ha";
+		for(let i = 0; i < 1; i++){
+			ha += "ha";
+		}
+	}
+}
+console.log(ha);
+
+// A simple while loop
+ha = "";
+let counter = 0;
+while(counter < 3){
+	ha += "ha";
+	counter += 1;
+}
+console.log(ha);
+
+// A messed up nested while loop
+ha = "";
+let counter1 = 0;
+while(counter1 < 1){
+	counter2 = 0;
+	ha += "ha";
+	while(counter2 < 1){
+		counter3 = 0;
+		ha += "ha";
+		while(counter3 < 1){
+			ha += "ha";
+			counter3 += 1;
+		}
+		counter2 += 1;
+	}
+	counter1 += 1;
 }
 console.log(ha);
