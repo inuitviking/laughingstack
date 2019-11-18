dd67d167ed0d9b8d7ef0b72d85a0c88f32e6c996
mplichta <bigcatplichta@gmail.com>
2019-11-18 16:02:23 -0700

    "added methods to laugh, chuckle, and giggle"

--- Ruby/hahaha.rb
+++ Ruby/hahaha.rb
@@ -1,4 +1,23 @@
 # Ruby 2.6.1
-3.times do
+
+# call below method with laugh and add optional argument. If no argument provided, will laugh 3 times
+# laugh 6 => "hahahahahaha", laugh 4 => "hahahaha", laugh => "hahaha"
+def laugh(funny_scale=nil)
+	if funny_scale.is_a? Integer
+		funny_scale.times do
+			giggle
+		end
+	else
+		chuckle
+	end
+end
+
+def chuckle
+	3.times do
+		giggle
+	end
+end
+
+def giggle
 	print "ha"
 end
