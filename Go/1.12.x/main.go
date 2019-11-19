0b169b1e6668fcc7e621c9c7881f31f9b4934550
cszczepaniak <cszczepaniak@gmail.com>
2019-11-18 20:26:59 -0600

    add simple laugh and gomod

---
+++ Go/1.12.x/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/inuitviking/laughingstack/Go/1.12.x/simple"
+)
+
+func main() {
+	// Simple laugh
+	fmt.Println(simple.Laugh(10))
+}
---
+++ Go/1.12.x/simple/simple.go
@@ -0,0 +1,9 @@
+package simple
+
+// Laugh laughs
+func Laugh(n int) (ha string) {
+	for i := 0; i < n; i++ {
+		ha += `ha`
+	}
+	return
+}
