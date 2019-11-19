f88475e8fa14b5dcbcf35d5bceff4fd8816ddea4
JakeHillHub <jakehillgithub@gmail.com>
2019-11-19 13:59:29 -0700

    Added x86 assembly program which laughs an adequate amount

---
+++ x86/hahaha.s
@@ -0,0 +1,52 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15
+	.intel_syntax noprefix
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register rbp
+	sub	rsp, 32
+	mov	dword ptr [rbp - 4], 0
+	mov	dword ptr [rbp - 8], edi
+	mov	qword ptr [rbp - 16], rsi
+	mov	dword ptr [rbp - 20], 0
+LBB0_1:                                 ## =>This Inner Loop Header: Depth=1
+	cmp	dword ptr [rbp - 20], 3
+	jge	LBB0_4
+## %bb.2:                               ##   in Loop: Header=BB0_1 Depth=1
+	lea	rdi, [rip + L_.str]
+	mov	al, 0
+	call	_printf
+	mov	dword ptr [rbp - 24], eax ## 4-byte Spill
+## %bb.3:                               ##   in Loop: Header=BB0_1 Depth=1
+	mov	eax, dword ptr [rbp - 20]
+	add	eax, 1
+	mov	dword ptr [rbp - 20], eax
+	jmp	LBB0_1
+LBB0_4:
+	lea	rdi, [rip + L_.str.1]
+	mov	al, 0
+	call	_printf
+	mov	ecx, dword ptr [rbp - 4]
+	mov	dword ptr [rbp - 28], eax ## 4-byte Spill
+	mov	eax, ecx
+	add	rsp, 32
+	pop	rbp
+	ret
+	.cfi_endproc
+                                        ## -- End function
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"ha"
+
+L_.str.1:                               ## @.str.1
+	.asciz	"\n"
+
+
+.subsections_via_symbols
