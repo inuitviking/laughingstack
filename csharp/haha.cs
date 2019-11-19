ffa3e698970fc22e71eb95f9d49ba87839acf2ee
Joefficial <43022878+Joefficial@users.noreply.github.com>
2019-11-18 23:03:00 -0500

    Create file that prints haha in csharp

---
+++ csharp/haha.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace haha
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Haha");
+        }
+    }
+}
