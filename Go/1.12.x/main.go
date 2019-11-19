83127289af450a6de325fae61f8c83ff3fee4d0f
cszczepaniak <cszczepaniak@gmail.com>
2019-11-18 21:03:22 -0600

    all the laughs

--- Go/1.12.x/main.go
+++ Go/1.12.x/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"github.com/inuitviking/laughingstack/Go/1.12.x/concurrent"
+	"github.com/inuitviking/laughingstack/Go/1.12.x/laugher"
 	"github.com/inuitviking/laughingstack/Go/1.12.x/simple"
 )
 
 func main() {
 	// Simple laugh
-	l := simple.SimpleLaugher{}
-	l.Guffaw()
+	allLaughs(simple.SimpleLaugher{})
 
 	// Concurrent laugh
+	allLaughs(concurrent.ConcurrentLaugher{})
+}
+
+func allLaughs(l laugher.Laugher) {
+	l.Chuckle()
+	l.Chortle()
+	l.Guffaw()
 }
