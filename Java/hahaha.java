abaec1ded2ab37a1a65e04d6064288aa13177ded
mfintonis <67mfintonis@gmail.com>
2019-11-19 11:50:26 -0500

    Fixed whitespace in Java file

--- Java/hahaha.java
+++ Java/hahaha.java
@@ -3,13 +3,13 @@ public class Main
 	public static void main(String[] args) {
 		System.out.print(Hahaha());
 	}
-	
+
 	public static String Hahaha() {
-    	 String haha = "";
-		 for(int i = 0; i < 3; i ++) {
-	           haha += "ha";
-    	 }
-		 
-		 return haha;
+		String haha = "";
+		for(int i = 0; i < 3; i ++) {
+			haha += "ha";
+		}
+
+		return haha;
 	}
 }
